0c5456763eb0cd2cc4d4a76448fc279589a98964
josiuuuuu <joeyjoeyhi5ivegood@outlook.kr>
2022-07-16 17:50:43 +0900

    analog

--- tutorial/tutorial.ino
+++ tutorial/tutorial.ino
@@ -1,10 +1,21 @@
 void setup() {
-  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT);
 
 }
 
 void loop() {
-  digitalWrite(7,HIGH);
-  delay(500)
-  digitalWrite(7,Low);
+  digitalWrite(8,HIGH);
+  delay(100);/*
+  digitalWrite(4,HIGH);
+  delay(100);
+  digitalWrite(2,HIGH);
+  delay(500);*/
+  digitalWrite(8,LOW);
+  delay(100);/*
+  digitalWrite(4,LOW);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(500);*/
 }
