f440c474f075b954c993c4f282c7f2956b3ba621
josiuuuuu <joeyjoeyhi5ivegood@outlook.kr>
2022-07-16 18:11:33 +0900

    서서히 껏다키기

--- analog_led/analog_led.ino
+++ analog_led/analog_led.ino
@@ -1,9 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(7,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    /*digitalWrite(7,HIGH);
+    delay(100);
+    digitalWrite(7,LOW);
+    delay(100);*/
+    for(int i = 0; i < 256; i++){
+      analogWrite(3,i);
+      delay(10);
+    }
+    
+    for(int i = 255; i >= 1; i--){
+      analogWrite(3,i);
+      delay(10);
+    }
+    
 }
