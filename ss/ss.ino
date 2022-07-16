310d528443d6b737c2b26a9716dce4f67d553d34
josiuuuuu <joeyjoeyhi5ivegood@outlook.kr>
2022-07-16 18:49:50 +0900

    밝기에 따라 불 켜기

--- ss/ss.ino
+++ ss/ss.ino
@@ -1,9 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int lightValue = analogRead(A0);
+  Serial.println(lightValue);
 
+  if(lightValue < 200){
+    digitalWrite(7, HIGH);
+  }
+  else{
+    digitalWrite(8, LOW);
+  }
+  delay(1000);
 }
